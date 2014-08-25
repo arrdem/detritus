b3c9ea857e2b62fd3474e865a567b05a79e060ae
arrdem <me@arrdem.com>
2014-08-25 09:02:38 -0500

    Add docstrings to the ->>asserts

--- src/detritus/pred.clj
+++ src/detritus/pred.clj
@@ -1,6 +1,17 @@
 (ns detritus.pred)
 
-(defn ->ensure
+(defn ->assert
+  "(λ T → ((λ T) → Bool)) → T
+  (λ T → ((λ T) → Bool) → String) → T
+
+  Function of a value, a predicate and optionally a string. If the
+  predicate is not true of the value, this function will assert false
+  giving the optional string or \"->assert failed!\" as the failure
+  message. Otherwise returns the argument value.
+
+  Example:
+    (-> 1 inc inc dec
+       (->assert number? \"oh god the types\"))"
   ([v pred]
      (->ensure v pred "->assert failed!"))
 
@@ -9,7 +20,18 @@
          (assert false error))))
 
 
-(defn ->>ensure
+(defn ->>assert
+  "(λ ((λ T) → Bool) → T) → T
+  (λ ((λ T) → Bool) → T → String) → T
+
+  Function of a predicate, optionally a string and a value. If the
+  predicate is not true of the value, this function will assert false
+  giving the optional string or \"->assert failed!\" as the failure
+  message. Otherwise returns the argument value.
+
+  Example:
+    (->> 1 inc inc dec
+      (->>assert number? \"oh god the types\"))"
   ([pred v]
      (->>ensure pred "->>assert failed!" v))
 
