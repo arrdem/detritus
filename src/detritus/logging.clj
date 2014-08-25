af277b272a35eb9bf324dc36c07337ba7ac7c87b
arrdem <me@arrdem.com>
2014-08-25 02:24:50 -0500

    Somewhat less awful log

--- src/detritus/logging.clj
+++ src/detritus/logging.clj
@@ -1,6 +1,14 @@
 (ns detritus.logging
-  "This namespace provides a trivial ")
+  "This namespace provides a trivial logging engine which uses
+  detritus.text for minimal formatting.
 
+  Note that the API here is likely very volatile and subject to
+  change due to being bare bones to the point of trivial."
+  (:require [detritus.text :as text]))
+
+
+;; Logging levels
+;;--------------------------------------------------------------------
 ;; FATAL    : 5
 ;; ERROR    : 4
 ;; WARN     : 3
@@ -8,36 +16,59 @@
 ;; INFO     : 1
 ;; DEBUG    : 0
 
+
+;; Dynamic configuration stuff [HACK]
+;;--------------------------------------------------------------------
+
 (def ^:dynamic *log-level*  -1)
 (def ^:dynamic *log-width*  80)
 (def ^:dynamic *log-stream* *err*)
 
+
+;; Logging API
+;;--------------------------------------------------------------------
+
+(defn do-print
+  [prefix args]
+  (->> args
+     (apply str)
+     (text/wrap-lines 70)
+     (map (fn [line] (str prefix line "\n")))
+     (apply str)
+     println))
+
+
 (defn debug [& args]
   (when (>= 0 *log-level*)
     (with-bindings {#'clojure.core/*out* *log-stream*}
-      (println "[DEBUG   ]" (apply str args)))))
+      (do-print "[DEBUG   ]" args))))
+
 
 (defn info [& args]
   (when (>= 1 *log-level*)
     (with-bindings {#'clojure.core/*out* *log-stream*}
-      (println "[INFO    ]" (apply str args)))))
+      (do-print "[INFO    ]" args))))
 
-(defn message [args]
+
+(defn message [& args]
   (when (>= 2 *log-level*)
     (with-bindings {#'clojure.core/*out* *log-stream*}
-      (println "[MESSAGE ]" (apply str args)))))
+      (do-print "[MESSAGE ]" args))))
 
-(defn warn [args]
+
+(defn warn [& args]
   (when (>= 3 *log-level*)
     (with-bindings {#'clojure.core/*out* *log-stream*}
-      (println "[WARN    ]" (apply str args)))))
+      (do-print "[WARN    ]" args))))
 
-(defn error [args]
+
+(defn error [& args]
   (when (>= 4 *log-level*)
     (with-bindings {#'clojure.core/*out* *log-stream*}
-      (println "[ERROR   ]" (apply str args)))))
+      (do-print "[ERROR   ]" args))))
+
 
 (defn fatal [args]
   (when (>= 5 *log-level*)
     (with-bindings {#'clojure.core/*out* *log-stream*}
-      (println "[FATAL   ]" (apply str args)))))
+      (do-print "[FATAL   ]" args))))
