7fcccbf4e16b8afb86dd70f651886c435a5cbeb8
arrdem <me@arrdem.com>
2015-01-29 17:44:55 -0600

    Comments

--- src/detritus/variants.clj
+++ src/detritus/variants.clj
@@ -5,7 +5,8 @@
   {:doc "Defines a variant constructor with a namespace qualified tag, and keyword named members."
    :arglists '([name doc-string? attr-map? members])}
   [vname & args]
-  (let [[?docstring args] (take-when string? "" args)
+  (let [;; Parse direct args
+        [?docstring args] (take-when string? "" args)
         [?attr-map args]  (take-when map? {} args)
         members           (first args)
 
@@ -13,6 +14,7 @@
         _                 (assert (vector? members) "Members is not a vector!")
         _                 (assert (every? symbol? members) "Members may contain only symbols!")
 
+        ;; Build used datastructures
         kw-members        (map (comp keyword name) members)
         kw-tag            (keyword (name (ns-name *ns*))
                                    (name vname))
