ac6751f400057ce1040c45d63fde0750ef9adaf2
arrdem <me@arrdem.com>
2015-01-29 16:50:52 -0600

    Add variants

---
+++ src/detritus/variants.clj
@@ -0,0 +1,24 @@
+(ns detritus.variants
+  (:require [detritus.update :refer [take-when]]))
+
+(defmacro defvariant
+  {:doc "Defines a variant constructor with a namespace qualified tag, and keyword named members."
+   :arglists '([name doc-string? attr-map? members])}
+  [vname & args]
+  (let [[?docstring args] (take-when string? "" args)
+        [?attr-map args]  (take-when map? {} args)
+        members           (first args)
+
+        ;; FIXME inline guards are a bad habit of mine
+        _                 (assert (vector? members) "Members is not a vector!")
+        _                 (assert (every? symbol? members) "Members may contain only symbols!")
+
+        kw-members        (map (comp keyword name) members)
+        kw-tag            (keyword (name (ns-name *ns*))
+                                   (name vname))
+        ?attr-map         (assoc ?attr-map
+                                 :variant true
+                                 :members kw-members
+                                 :tag kw-tag)]
+    `(defn ~vname ~?docstring ~?attr-map ~members
+       [~kw-tag (hash-map ~@(interleave kw-members members))])))
