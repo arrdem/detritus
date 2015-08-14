8e50e8d48c3d08e9df9b00397234ad0ede1f7316
Reid 'arrdem' McKenzie <reid@factual.com>
2015-08-14 12:04:18 -0700

    [NO TESTS] WIP

--- src/detritus/string.clj
+++ src/detritus/string.clj
@@ -1,5 +1,19 @@
 (ns detritus.string)
 
+(defn remove-from-end [s end]
+  (when s
+    (if (.endsWith s end)
+      (.substring s 0 (- (count s)
+                         (count end)))
+      s)))
+
+(defn re-find-last [re s]
+  (let [m (re-matcher re s)]
+    (loop [p nil]
+      (if-let [x (re-find m)]
+        (recur x)
+        p))))
+
 (defn re-fn
   "Construct a regular expression from string.  Calling a regular
   expression with no arguments returns a Pattern.  Calling a regular
