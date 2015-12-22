8e5dad2df6c6ff59a95b98620d44b89b8efff83d
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-12-22 15:14:35 -0600

    Try another version structure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.arrdem/detritus "0.2.2"
+(defproject me.arrdem/detritus (slurp "resources/detritus/VERSION")
   :description "Arrdem's util library"
   :url "http://github.com/arrdem/detritus"
   :license {:name "Eclipse Public License"
