16d3085806a77402a558f9781b59d505bdb4b554
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-12-22 16:09:30 -0600

    Whitespace

--- src/detritus/logging.clj
+++ src/detritus/logging.clj
@@ -6,7 +6,6 @@
   change due to being bare bones to the point of trivial."
   (:require [detritus.text :as text]))
 
-
 ;; Logging levels
 ;;--------------------------------------------------------------------
 ;; FATAL    : 5
@@ -16,14 +15,12 @@
 ;; INFO     : 1
 ;; DEBUG    : 0
 
-
-;; Dynamic configuration stuff [HACK]
+;; Dynamic configuration stuff
 ;;--------------------------------------------------------------------
 
 (def ^:dynamic *log-level*  -1)
 (def ^:dynamic *log-width*  80)
 (def ^:dynamic *log-stream* *err*)
-
 
 ;; Logging API
 ;;--------------------------------------------------------------------
@@ -32,21 +29,20 @@
   [log-level message prefix args]
   (when (>= log-level *log-level*)
     (->> args
-       (map str)
-       (map text/lines)
-       (apply concat)
-       (map (partial (fn format-line [spacing line]
-                 (if (>= (+ (count prefix) (count line)) *log-width*)
-                   (->> line
-                      (text/wrap-lines (- *log-width* 7 (count prefix)))
-                      (map (partial format-line "     : "))
-                      (apply str))
-                   (str prefix spacing line "\n")))
-               "     "))
-       (cons (str prefix " [" message "]\n"))
-       (apply str)
-       print)))
-
+         (map str)
+         (map text/lines)
+         (apply concat)
+         (map (partial (fn format-line [spacing line]
+                         (if (>= (+ (count prefix) (count line)) *log-width*)
+                           (->> line
+                                (text/wrap-lines (- *log-width* 7 (count prefix)))
+                                (map (partial format-line "     : "))
+                                (apply str))
+                           (str prefix spacing line "\n")))
+                       "     "))
+         (cons (str prefix " [" message "]\n"))
+         (apply str)
+         print)))
 
 (defmacro defdebug [symbol level prefix]
   `(defmacro ~symbol [~'& args#]
@@ -57,7 +53,6 @@
                    (str "ns " (name (.name *ns*)) " | file " *file* ":" (:line meta#))
                    ~prefix
                    (vec args#))))))
-
 
 (defdebug debug   0 "[DEBUG   ]")
 (defdebug info    1 "[INFO    ]")
