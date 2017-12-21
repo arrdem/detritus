5c4252ce6f458ea2dc6fd8590374801b0f3ced58
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-12-20 21:35:16 -0800

    Cut over to lein-git-version

--- project.clj
+++ project.clj
@@ -1,6 +1,16 @@
-(defproject me.arrdem/detritus (slurp "resources/detritus/VERSION")
+(defproject me.arrdem/detritus "_"
   :description "Arrdem's util library"
   :url "http://github.com/arrdem/detritus"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+
+  :plugins [[me.arrdem/lein-git-version "LATEST"]]
+  :git-version {:status-to-version
+                (fn [{:keys [tag version ahead ahead? dirty?] :as git}]
+                  (if (and tag (not ahead?) (not dirty?))
+                    tag
+                    (str tag
+                         (when ahead? (str "." ahead))
+                         (when dirty? "-SNAPSHOT"))))})
