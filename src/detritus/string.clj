cb0d7808cbb53a6abaa1109dd7d1bf91b5bb5eb5
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-12-30 13:10:44 -0800

    Docstrings, indicies-of, prefixes-by-including

--- src/detritus/string.clj
+++ src/detritus/string.clj
@@ -1,13 +1,17 @@
 (ns detritus.string)
 
-(defn remove-from-end [s end]
+(defn remove-from-end
+  "Removes a strictly trailing substring."
+  [s end]
   (when s
     (if (.endsWith s end)
       (.substring s 0 (- (count s)
                          (count end)))
       s)))
 
-(defn re-find-last [re s]
+(defn re-find-last
+  "Returns the last match of `re` in `s` if any."
+  [re s]
   (let [m (re-matcher re s)]
     (loop [p nil]
       (if-let [x (re-find m)]
@@ -27,3 +31,19 @@
              (if (first groups)
                groups
                nil))))))
+
+(defn indicies-of [^String text ^String s]
+  (loop [acc  []
+         last (.indexOf text s)]
+    (if-not (= -1 last)
+      (recur (conj acc last) (.indexOf text s (inc last)))
+      acc)))
+
+(defn prefixes-by-including
+  "Returns all prefixes of the `text` which are delimited by an
+  occurrence of the provided string `s`.
+
+  Includes the delimiter in all prefixes."
+  [^String text ^String s]
+  (for [idx (reverse (indicies-of text s))]
+    (.substring text 0 (+ idx (count s)))))
