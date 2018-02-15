95200294357bcca219ea33293d77e42199aeef62
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-02-15 01:29:54 -0800

    Set up lein-git-version with deploy tasks

--- project.clj
+++ project.clj
@@ -6,11 +6,29 @@
 
   :dependencies [[org.clojure/clojure "1.9.0"]]
 
-  :plugins [[me.arrdem/lein-git-version "LATEST"]]
-  :git-version {:status-to-version
-                (fn [{:keys [tag version ahead ahead? dirty?] :as git}]
-                  (if (and tag (not ahead?) (not dirty?))
-                    tag
-                    (str tag
-                         (when ahead? (str "." ahead))
-                         (when dirty? "-SNAPSHOT"))))})
+  :plugins [[me.arrdem/lein-git-version "2.0.5"]
+            [me.arrdem/lein-auto "0.1.4"]
+            [lein-cljfmt "0.5.7"]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "tag"]
+                  ["vcs" "push"]
+                  ["deploy"]]
+
+  :git-version
+  {:status-to-version
+   (fn [{:keys [tag version branch ahead ahead? dirty?] :as git}]
+     (if (and tag (not ahead?) (not dirty?))
+       (do (assert (re-find #"\d+\.\d+\.\d+" tag)
+                   "Tag is assumed to be a raw SemVer version")
+           tag)
+       (if (and tag (or ahead? dirty?))
+         (let [[_ prefix patch] (re-find #"(\d+\.\d+)\.(\d+)" tag)
+               patch            (Long/parseLong patch)
+               patch+           (inc patch)]
+           (format "%s.%d%s-SNAPSHOT" prefix patch+
+                   (if (and (not= "master" branch)
+                            (not (.contains branch "/")))
+                     (str \- branch) "")))
+         "0.1.0-SNAPSHOT")))})
